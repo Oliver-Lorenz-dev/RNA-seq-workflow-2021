64c1c92c205587eb71e159f3ccb1cc3a3bd6f617
Oliver-Lorenz-dev <85516197+Oliver-Lorenz-dev@users.noreply.github.com>
2021-08-12 20:52:48 +0100

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -1,16 +1,32 @@
 #!/usr/bin/bash
 
-# download data
+
+# data download
 wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR343/000/SRR3437050/SRR3437050_1.fastq.gz
 wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR343/000/SRR3437050/SRR3437050_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR343/009/SRR3437049/SRR3437049_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR343/009/SRR3437049/SRR3437049_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR343/002/SRR3437052/SRR3437052_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR343/002/SRR3437052/SRR3437052_2.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR343/001/SRR3437051/SRR3437051_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR343/001/SRR3437051/SRR3437051_2.fastq.gz
 
-# quality control
+# QC
+mkdir QC
+
 fastqc *fastq.gz -o QC
 
 multiqc QC
 
-# preprocessing
-fastp -i SRR3437050_1.fastq.gz -I SRR3437050_2.fastq.gz -o processed_reads/SRR3437050_1.fastq.gz -O processed_reads/SRR3437050_2.fastq.gz --detect_adapter_for_pe -c
+# pre processing
+for index in {49..52}
+do
+       fastp -i SRR34370"${index}"_1.fastq.gz -I SRR34370"${index}"_2.fastq.gz -o processed_reads/SRR34370"${index}"_1.fastq.gz -O processed_reads/SRR34370"${index}"_2.fastq.gz --detect_adapter_for_pe -c -R "SRR34370"${index}"" -h "SRR34370"${index}".html"
+done
 
 # read quantification
-salmon quant -i processed_reads/human_v38_index -l A -1 processed_reads/SRR3437050_1.fastq.gz -2 processed_reads/SRR3437050_2.fastq.gz -o processed_reads/SRR3437050
+for index in {49..52}
+do
+        salmon quant -i processed_reads/human_v38_index -l A -1 processed_reads/SRR34370"${index}"_1.fastq.gz -2 processed_reads/SRR34370"${index}"_2.fastq.gz -o processed_reads/SRR34370"${index}"
+done
+
